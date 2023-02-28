f351908901e4c00071f7bdec4c3b3c36562f5c8c
solomiyasabat <solomiyasabat@gmail.com>
2023-02-28 18:46:21 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Input_Bar from "./Input_Bar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">✅ To Do List</header>
+      <Input_Bar />
+    </div>
+  );
+}
+
+export default App;
